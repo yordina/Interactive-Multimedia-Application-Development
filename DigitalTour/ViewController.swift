8bc25c07076ff3eb4bf19800d3352959362e35fa
Yordanos Mogos <yorda@sfs-wifi-dhcp-10-143-42-228.wifi.sfsu.edu>
2019-05-16 17:53:08 -0700

    Added Capital Cities

--- DigitalTour/ViewController.swift
+++ DigitalTour/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, MKMapViewDelegate,SFSpeechRecognizerDele
 
     
     
-    var userInputLocation = FlyoverAwesomePlace.parisEiffelTower
+    var userInputLocation =  CLLocationCoordinate2D(latitude: 40.7484405, longitude: -73.9856644)
     let speechRec: SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale.init(identifier : "en-us"))
     var recognitionRequest:SFSpeechAudioBufferRecognitionRequest?
     var recognitionTask: SFSpeechRecognitionTask?
@@ -94,7 +94,7 @@ class ViewController: UIViewController, MKMapViewDelegate,SFSpeechRecognizerDele
                     self.userInputLocation = self.locationDictionary[bestTrans!]!
                 } else{
                     self.tourLabel.text = "Can't find this place"
-                    self.userInputLocation = FlyoverAwesomePlace.newYorkStatueOfLiberty
+                    self.userInputLocation = CLLocationCoordinate2D(latitude: 15.3229, longitude: 38.9251)
                 }
                   self.mapSetup()
                 }
@@ -152,17 +152,39 @@ class ViewController: UIViewController, MKMapViewDelegate,SFSpeechRecognizerDele
         self.tourMapKit.isZoomEnabled = true
         self.tourMapKit.isScrollEnabled = true
 
-        let camera = FlyoverCamera(mapView: self.tourMapKit, configuration:FlyoverCamera.Configuration(duration: 6.0, altitude: 300, pitch: 45.0, headingStep: 20.0))
+        let camera = FlyoverCamera(mapView: self.tourMapKit, configuration:FlyoverCamera.Configuration(duration: 20.0, altitude: 600, pitch: 45.0, headingStep: 40.0))
         camera.start(flyover: self.userInputLocation)
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(9), execute: {camera.stop()})
     }
-    let locationDictionary = ["Statue of Liberty": FlyoverAwesomePlace.newYorkStatueOfLiberty,
-                              "Golden gate bridge" : FlyoverAwesomePlace.sanFranciscoGoldenGateBridge,
-                              "Big ben":FlyoverAwesomePlace.londonBigBen,
-                              "Eiffel Tower":FlyoverAwesomePlace.parisEiffelTower,
-                              "Sydney Opera house": FlyoverAwesomePlace.sydneyOperaHouse,
-                              "Rome": FlyoverAwesomePlace.romeColosseum,
-                              "Apple": FlyoverAwesomePlace.appleHeadquarter]
+//    let locationDictionary = ["Statue of Liberty": FlyoverAwesomePlace.newYorkStatueOfLiberty,
+//                              "Golden gate bridge" : FlyoverAwesomePlace.sanFranciscoGoldenGateBridge,
+//                              "Big ben":FlyoverAwesomePlace.londonBigBen,
+//                              "Eiffel Tower":FlyoverAwesomePlace.parisEiffelTower,
+//                              "Sydney Opera house": FlyoverAwesomePlace.sydneyOperaHouse,
+//                              "Rome": FlyoverAwesomePlace.romeColosseum,
+//                              "Apple": FlyoverAwesomePlace.appleHeadquarter]
+    let locationDictionary = ["New York": CLLocationCoordinate2D(latitude: 40.7484405, longitude: -73.9856644),
+                              "Asmara": CLLocationCoordinate2D(latitude: 15.3229, longitude: 38.9251),
+        "Kabul" :    CLLocationCoordinate2D(latitude: 34.28, longitude: 69.11),
+        "Buenos Aires":  CLLocationCoordinate2D(latitude: 36.30, longitude: -60.00),
+        "Brussels" :  CLLocationCoordinate2D(latitude: 50.51, longitude: 04.21),
+        //Brasilia    15.47S    47.55W
+        //Santiago    33.24S    70.40W
+        //Beijing    39.55N    116.20E
+        //Havana    23.08N    82.22W
+        //Copenhagen    55.41N    12.34E
+        //Djibouti    11.08N    42.20E
+        "Addis Ababa" :    CLLocationCoordinate2D(latitude: 09.02, longitude: 38.42)
+        //Paris    48.50N    02.20E
+        //Athens    37.58N    23.46E
+        //Rome    41.54N    12.29E
+        //Mexico    19.20N    99.10W
+        //Berlin    52.30N    13.25E
+        //Madrid    40.25N    03.45W
+        //Washington DC    39.91N    77.02W
+        //Khartoum    15.31N    32.35E
+        //Riyadh    24.41N    46.42E
+    ]
     
 }
 
